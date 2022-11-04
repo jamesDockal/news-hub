19c6dd7f2b097d38d82e1f60fedca5465a71e85a
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-03 21:57:19 -0300

    chore: add hooks of push

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+const config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  transform: {
+    "^.+\\.(t|j)sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+            decorators: true,
+          },
+          keepClassNames: true,
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
+        module: {
+          type: "es6",
+          noInterop: false,
+        },
+      },
+    ],
+  },
+};
+export default config;
