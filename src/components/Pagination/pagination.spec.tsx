3b529253c2e1b92ca21e8d17ddf8c9517c359b62
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-04 10:09:16 -0300

    refactor: remove unnecessary code

--- src/components/Pagination/pagination.spec.tsx
+++ src/components/Pagination/pagination.spec.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { newsApi } from "../../services/newsApi";
 
 import { Pagination } from "./pagination.component";
 
@@ -43,7 +42,6 @@ describe("Home Page", () => {
     render(
       <Pagination
         onPageChange={(newPage) => {
-          console.log("newPage", newPage);
           currentPage = newPage;
         }}
         totalCountOfRegisters={totalCountOfRegisters}
