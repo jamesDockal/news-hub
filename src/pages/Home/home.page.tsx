b36847c0b12a549b4a1e81a4a68194cca59a5ab8
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-04 00:39:18 -0300

    feat: add Pagination component to Home page

--- src/pages/Home/home.page.tsx
+++ src/pages/Home/home.page.tsx
@@ -1,25 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "../../components/Card/card.component";
+import { Pagination } from "../../components/Pagination/pagination.component";
 import { Article } from "../../dto/article.dto";
 import { newsApi } from "../../services/newsApi";
 import { Articles } from "./home.styles";
 
+type HeadLine = {
+  articles: Article[];
+  totalResults: number;
+};
+
 export const Home: React.FC = () => {
-  const [articles, setArticles] = useState<Article[]>([]);
+  const [headlines, setHeadlines] = useState<HeadLine>({} as HeadLine);
+
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    newsApi.get("/top-headlines?country=br").then(({ data }) => {
-      setArticles(data.articles);
-    });
-  }, []);
-  console.log("articles", articles);
+    newsApi
+      .get(`/top-headlines?country=br&page=${currentPage}&pageSize=10`)
+      .then(({ data }) => {
+        console.log("data", data);
+        setHeadlines(data);
+      });
+  }, [currentPage]);
 
   return (
     <div>
       <Articles>
-        {articles.map((article) => (
+        {headlines.articles?.map((article) => (
           <Card article={article} key={article.title} />
         ))}
+
+        <Pagination
+          onPageChange={setCurrentPage}
+          totalCountOfRegisters={headlines.totalResults}
+          currentPage={currentPage}
+        />
       </Articles>
     </div>
   );
