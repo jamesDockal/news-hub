af440ecf866841c17ebc978f9567e34e9a7b13a1
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-04 00:38:58 -0300

    test: ensure Pagination component works as it should

---
+++ src/components/Pagination/pagination.spec.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { newsApi } from "../../services/newsApi";
+
+import { Pagination } from "./pagination.component";
+
+describe("Home Page", () => {
+  it("should render buttons with the number of the page", async () => {
+    const totalCountOfRegisters = 20;
+    const registersPerPage = 10;
+    render(
+      <Pagination
+        onPageChange={() => {}}
+        totalCountOfRegisters={totalCountOfRegisters}
+        registersPerPage={registersPerPage}
+      />
+    );
+
+    expect(screen.getByLabelText("page-1")).toBeInTheDocument();
+    expect(screen.getByLabelText("page-2")).toBeInTheDocument();
+  });
+
+  it("should render '...' if the count of register is too large", async () => {
+    const totalCountOfRegisters = 200;
+    const registersPerPage = 10;
+    render(
+      <Pagination
+        onPageChange={() => {}}
+        totalCountOfRegisters={totalCountOfRegisters}
+        registersPerPage={registersPerPage}
+      />
+    );
+    const elipses = screen.getAllByText("...");
+    elipses.forEach((elipse) => {
+      expect(elipse).toBeInTheDocument();
+    });
+  });
+
+  it("should change the current page on button click", async () => {
+    const totalCountOfRegisters = 200;
+    const registersPerPage = 10;
+    let currentPage = 1;
+    render(
+      <Pagination
+        onPageChange={(newPage) => {
+          console.log("newPage", newPage);
+          currentPage = newPage;
+        }}
+        totalCountOfRegisters={totalCountOfRegisters}
+        registersPerPage={registersPerPage}
+        currentPage={currentPage}
+      />
+    );
+
+    const button2 = screen.getByLabelText("page-2");
+    userEvent.click(button2);
+
+    expect(currentPage).toBe(2);
+  });
+});
