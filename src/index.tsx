94a00be26d26c04155edc2ca398dd1ed8220cab5
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-03 23:32:43 -0300

    feat: create global theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import "./index.css";
 import { Home } from "./pages/Home/home.page";
+import { theme } from "./styles/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <Home />
-  </React.StrictMode>
+  </ThemeProvider>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,6 @@
+export const theme = {
+  pink: "#D53F8C",
+  gray_900: "#181C22",
+  gray_400: "#797D9A",
+  gray_50: "#EEEEF2",
+};
