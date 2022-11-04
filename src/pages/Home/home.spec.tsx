c7adf0709e199696b6091fe25294d87005bd195c
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-03 23:35:57 -0300

    test: ensure homepage render articles

--- src/pages/Home/home.spec.tsx
+++ src/pages/Home/home.spec.tsx
@@ -1,14 +1,11 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { render, screen } from "@testing-library/react";
 import { newsApi } from "../../services/newsApi";
-import ReactDOM from "react-dom/client";
 
 import { Home } from "./home.page";
+import { Article } from "../../dto/article.dto";
 
 describe("Home Page", () => {
   it("should call news api", async () => {
-    jest.useFakeTimers();
-
     const spy = jest.spyOn(newsApi, "get").mockImplementation(async () => {
       return await new Promise((resolve) =>
         resolve({
@@ -20,5 +17,25 @@ describe("Home Page", () => {
     render(<Home />);
 
     expect(spy).toHaveBeenCalled();
+  });
+
+  it("should render Card component with title for each article", async () => {
+    jest.spyOn(newsApi, "get").mockImplementation(async () => {
+      return await new Promise((resolve) =>
+        resolve({
+          data: {
+            articles: [
+              {
+                title: "any_title",
+              } as Article,
+            ],
+          },
+        })
+      );
+    });
+
+    render(<Home />);
+
+    expect(await screen.findByText("any_title")).toBeInTheDocument();
   });
 });
