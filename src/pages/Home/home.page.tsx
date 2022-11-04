4a906ee923a8c283562f22d7dd2adf988e7c5f47
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-04 01:00:28 -0300

    feat: add loading component to home page

--- src/pages/Home/home.page.tsx
+++ src/pages/Home/home.page.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "../../components/Card/card.component";
+import { Loading } from "../../components/Loading/loading.component";
 import { Pagination } from "../../components/Pagination/pagination.component";
 import { Article } from "../../dto/article.dto";
 import { newsApi } from "../../services/newsApi";
@@ -12,31 +13,36 @@ type HeadLine = {
 
 export const Home: React.FC = () => {
   const [headlines, setHeadlines] = useState<HeadLine>({} as HeadLine);
-
   const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     newsApi
       .get(`/top-headlines?country=br&page=${currentPage}&pageSize=10`)
       .then(({ data }) => {
-        console.log("data", data);
+        setIsLoading(false);
         setHeadlines(data);
       });
   }, [currentPage]);
 
   return (
     <div>
-      <Articles>
-        {headlines.articles?.map((article) => (
-          <Card article={article} key={article.title} />
-        ))}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Articles>
+          {headlines.articles?.map((article) => (
+            <Card article={article} key={article.title} />
+          ))}
 
-        <Pagination
-          onPageChange={setCurrentPage}
-          totalCountOfRegisters={headlines.totalResults}
-          currentPage={currentPage}
-        />
-      </Articles>
+          <Pagination
+            onPageChange={setCurrentPage}
+            totalCountOfRegisters={headlines.totalResults}
+            currentPage={currentPage}
+          />
+        </Articles>
+      )}
     </div>
   );
 };
